27073e4536b4af1cbf8602dfc9c9696abde8df1f
zliel <zpliel@gmail.com>
2022-04-02 03:14:09 -0700

    Added a Cleanup bean to delete the Courses added by the InitDatabase bean to avoid clutter

---
+++ src/main/java/com/personal/springbootpractice/Cleanup.java
@@ -0,0 +1,32 @@
+package com.personal.springbootpractice;
+
+import com.personal.springbootpractice.models.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
+import java.util.ArrayList;
+
+@Component
+public class Cleanup {
+
+    @Autowired
+    MongoOperations ops;
+
+    @PreDestroy
+    public void cleanDB() {
+        Query queryOne = new Query().addCriteria(Criteria.where("courseName").is("Newly Made Course!"));
+        Query queryTwo = new Query().addCriteria(Criteria.where("courseName").is("Newly Made Course 2.0!"));
+        Query queryThree = new Query().addCriteria(Criteria.where("courseName").is("This is an old course lol!"));
+
+        ArrayList<Course> deletedCourses = new ArrayList<>();
+        deletedCourses.addAll(ops.findAllAndRemove(queryOne, "course"));
+        deletedCourses.addAll(ops.findAllAndRemove(queryTwo, "course"));
+        deletedCourses.addAll(ops.findAllAndRemove(queryThree, "course"));
+
+        System.out.println(deletedCourses);
+    }
+}
