49685113828c4c1862458b94d5f4ffd1cd23ac30
zliel <zpliel@gmail.com>
2022-04-10 00:18:31 -0700

    Temporarily removed the "/login" GET mapping function which conflicted with Spring Security's login page

--- src/main/java/com/personal/springbootpractice/controllers/LoginController.java
+++ src/main/java/com/personal/springbootpractice/controllers/LoginController.java
@@ -1,18 +1,16 @@
 package com.personal.springbootpractice.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import reactor.core.publisher.Mono;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
-    public Mono<String> login() { return Mono.just("Login"); }
+
 
     @PostMapping("/login")
     public Mono<String> submitLogin() {
-        return Mono.just("No functionality quite yet.");
+        return Mono.just("/");
     }
  }
