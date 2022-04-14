20c213bff87a7394fb7dde67aba60a69f3f8e7c8
zliel <zpliel@gmail.com>
2022-04-14 02:24:55 -0700

    Changed the "User" model to implement Spring Security's UserDetails specification

--- src/main/java/com/personal/springbootpractice/models/User.java
+++ src/main/java/com/personal/springbootpractice/models/User.java
@@ -1,29 +1,62 @@
 package com.personal.springbootpractice.models;
 
+import lombok.Builder;
 import lombok.Data;
-import org.bson.types.ObjectId;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Document
-public class User {
+@ToString
+public class User implements UserDetails {
 
     @Id
-    private String id;
     private String username;
     private String password;
     private String schoolName;
 
+    private boolean active = true;
+    private Set<GrantedAuthority> roles = new HashSet<>();
+
+    @Builder
     public User(String username, String password, String schoolName) {
-        this.id = new ObjectId().toString();
         this.username = username;
         this.password = password;
         this.schoolName = schoolName;
+        this.roles.add(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
-    public User() {
-        this.id = new ObjectId().toString();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return active;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return active;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return active;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
 }
