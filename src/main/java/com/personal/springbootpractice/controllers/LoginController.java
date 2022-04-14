5beef8ff06e46cf2bce106ba6a6076a481f77e6b
zliel <zpliel@gmail.com>
2022-04-14 03:29:04 -0700

    Changed the "/login" route to use a custom login page instead of the base Spring Security page

--- src/main/java/com/personal/springbootpractice/controllers/LoginController.java
+++ src/main/java/com/personal/springbootpractice/controllers/LoginController.java
@@ -1,16 +1,14 @@
 package com.personal.springbootpractice.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import reactor.core.publisher.Mono;
 
 @Controller
 public class LoginController {
 
+    @GetMapping("/login")
+    public Mono<String> login() { return Mono.just("Login"); }
 
-
-    @PostMapping("/login")
-    public Mono<String> submitLogin() {
-        return Mono.just("/");
-    }
  }
