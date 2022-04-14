7b077557a877422f4caad1cb6f2b9f5fa7388882
zliel <zpliel@gmail.com>
2022-04-14 13:16:11 -0700

    Added a check in the "/login" mapping for whether the user is already logged in, redirecting them to the home page if so.

--- src/main/java/com/personal/springbootpractice/controllers/LoginController.java
+++ src/main/java/com/personal/springbootpractice/controllers/LoginController.java
@@ -1,14 +1,24 @@
 package com.personal.springbootpractice.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import reactor.core.publisher.Mono;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public Mono<String> login() { return Mono.just("Login"); }
+    public Mono<String> login() {
+        return ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .map(Authentication::isAuthenticated)
+                .flatMap(loggedIn -> {
+                    if(loggedIn) { return Mono.just("redirect:/"); }
+                    return Mono.just("Login");
+                });
+        }
 
  }
