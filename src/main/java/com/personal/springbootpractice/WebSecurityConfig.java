738585b4f8f5cf7b86c9abd7dcf84ab608bcf930
zliel <zpliel@gmail.com>
2022-04-14 02:19:37 -0700

    Added a method to validate login requests

--- src/main/java/com/personal/springbootpractice/WebSecurityConfig.java
+++ src/main/java/com/personal/springbootpractice/WebSecurityConfig.java
@@ -1,31 +1,45 @@
 package com.personal.springbootpractice;
 
+import com.personal.springbootpractice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class WebSecurityConfig {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http.authorizeExchange()
-                .pathMatchers("/", "/allcourses", "/styles/**", "/images/**")
-                .permitAll()
-                .and()
-            .authorizeExchange()
                 .pathMatchers("/courses/delete/**", "/courses/edit/**")
                 .hasRole("ADMIN")
+                .and()
+            .authorizeExchange()
+                .pathMatchers("/allcourses")
+                .hasAnyRole("USER", "ADMIN")
+                .and()
+            .authorizeExchange()
+                .pathMatchers("/", "/login", "/signup", "/styles/**", "/images/**")
+                .permitAll()
                 .and()
             .formLogin()
                 .and()
             .logout();
+
         return http.build();
+    }
+
+
+    @Bean
+    public ReactiveUserDetailsService userDetailsService() {
+        return (username) -> userRepository.findByUsername(username);
     }
 }
