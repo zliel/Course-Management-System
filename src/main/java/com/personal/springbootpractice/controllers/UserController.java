3f29a47a87a88d2c967468c2c5b2fe6a39b8b2c2
zliel <zpliel@gmail.com>
2022-04-14 22:07:55 -0700

    Made a controller to handle serving the signup page and saving new users to the database

---
+++ src/main/java/com/personal/springbootpractice/controllers/UserController.java
@@ -0,0 +1,41 @@
+package com.personal.springbootpractice.controllers;
+
+import com.personal.springbootpractice.models.User;
+import com.personal.springbootpractice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @GetMapping("/signup")
+    public Mono<String> signup(Model model) {
+        model.addAttribute("user", new User());
+        return Mono.just("Signup");
+    }
+
+    @PostMapping("/users/new")
+    public Mono<String> createNewUser(@ModelAttribute(value="user") User newUser) {
+        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+
+        return userRepository.existsUserByUsername(newUser.getUsername())
+               .flatMap(userExists -> {
+                    if(userExists) return Mono.just("redirect:/");
+                    return userRepository.save(newUser).then(Mono.just("redirect:/login"));
+        });
+
+    }
+}
