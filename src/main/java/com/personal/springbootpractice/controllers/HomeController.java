24c128bf553b6736805c2fb6578450a903dec3de
zliel <zpliel@gmail.com>
2022-04-17 01:37:09 -0700

    Added an attribute to the home page to determine whether to show the login/signup buttons or the log out button

--- src/main/java/com/personal/springbootpractice/controllers/HomeController.java
+++ src/main/java/com/personal/springbootpractice/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.personal.springbootpractice.controllers;
 
+import com.personal.springbootpractice.util.AuthenticationUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import reactor.core.publisher.Mono;
 
@@ -8,7 +10,8 @@ import reactor.core.publisher.Mono;
 public class HomeController {
 
     @GetMapping("/")
-    public Mono<String> home() {
+    public Mono<String> home(Model model) {
+        model.addAttribute("isAuthenticated", AuthenticationUtils.isAuthenticated());
         return Mono.just("Home");
     }
 }
