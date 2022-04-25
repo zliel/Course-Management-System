0aff149f8f56047a23ce6b35a545259b0c1a8d3b
zliel <zpliel@gmail.com>
2022-04-24 20:50:58 -0700

    Added a method to find courses by their associated schools

--- src/main/java/com/personal/springbootpractice/repositories/CourseRepository.java
+++ src/main/java/com/personal/springbootpractice/repositories/CourseRepository.java
@@ -3,9 +3,12 @@ package com.personal.springbootpractice.repositories;
 
 import com.personal.springbootpractice.models.Course;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
 public interface CourseRepository extends ReactiveCrudRepository<Course, Long> {
     Mono<Course> findById(String id);
+
+    Flux<Course> findAllBySchoolName(String schoolName);
 }
