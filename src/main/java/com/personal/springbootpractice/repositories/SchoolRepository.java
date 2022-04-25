2f7b7cfee2d72e0abda8a92ab96c24ecc117448e
zliel <zpliel@gmail.com>
2022-04-25 00:50:45 -0700

    Added a method for finding Schools by their IDs

--- src/main/java/com/personal/springbootpractice/repositories/SchoolRepository.java
+++ src/main/java/com/personal/springbootpractice/repositories/SchoolRepository.java
@@ -3,7 +3,9 @@ package com.personal.springbootpractice.repositories;
 
 import com.personal.springbootpractice.models.School;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 
-public interface SchoolRepository extends ReactiveCrudRepository<School, Long> {
+public interface SchoolRepository extends ReactiveCrudRepository<School, String> {
+    Mono<School> findById(String id);
 }
