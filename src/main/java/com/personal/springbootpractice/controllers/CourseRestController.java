75ba311fbe7423382a2e6c430d19cd17c3bc7aee
zliel <zpliel@gmail.com>
2022-05-04 23:28:01 -0700

    Added methods for getting all courses and getting courses by their associated school, and changed the add/delete course mappings to POST instead of GET

--- src/main/java/com/personal/springbootpractice/controllers/CourseRestController.java
+++ src/main/java/com/personal/springbootpractice/controllers/CourseRestController.java
@@ -4,7 +4,9 @@ import com.personal.springbootpractice.models.Course;
 import com.personal.springbootpractice.repositories.CourseRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
@@ -20,14 +22,23 @@ public class CourseRestController {
         this.repository = repository;
     }
 
+    @GetMapping("/api/courses")
+    public Flux<Course> getAllCourses() {
+        return repository.findAll();
+    }
+
     @GetMapping("/api/courses/{id}")
     public Mono<Course> getCourse(@PathVariable("id") Long id) {
-        Mono<Course> foundCourse = repository.findById(id);
-        return foundCourse;
+        return repository.findById(id);
+    }
+
+    @GetMapping("/api/courses/{schoolName}")
+    public Flux<Course> getCoursesBySchoolName(@PathVariable("schoolName") String schoolName) {
+        return repository.findAllBySchoolName(schoolName);
     }
 
     // This would be changed to a POST mapping
-    @GetMapping("/api/courses/new/{id}")
+    @PostMapping("/api/courses/new/{id}")
     public Mono<String> addNewCourse(@PathVariable("id") Long id) {
         Course courseToAdd = new Course("Newly Made Course!", new Date(), Date.from(Instant.now().plus(90, ChronoUnit.DAYS)), 30, "UCSD");
 
@@ -35,7 +46,7 @@ public class CourseRestController {
     }
 
     // This should be changed to a DELETE mapping, and the HttpHiddenFilter Bean might need to be added
-    @GetMapping("/api/courses/delete/{id}")
+    @PostMapping("/api/courses/delete/{id}")
     public Mono<String> deleteCourse(@PathVariable Long id) {
         Mono<Course> courseToDelete = repository.findById(id);
 
