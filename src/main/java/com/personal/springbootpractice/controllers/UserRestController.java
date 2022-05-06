a9ea4aaaf371f37807b880612d2804422e9b6d31
zliel <zpliel@gmail.com>
2022-05-06 01:24:25 -0700

    Added edit and delete functionality to the User REST Controller

--- src/main/java/com/personal/springbootpractice/controllers/UserRestController.java
+++ src/main/java/com/personal/springbootpractice/controllers/UserRestController.java
@@ -4,19 +4,26 @@ import com.personal.springbootpractice.models.User;
 import com.personal.springbootpractice.repositories.UserRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Api(tags = "Users")
 public class UserRestController {
 
     final UserRepository repository;
+    final PasswordEncoder passwordEncoder;
 
-    public UserRestController(UserRepository repository) { this.repository = repository; }
+    public UserRestController(UserRepository repository, PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
 
     @GetMapping("/api/users")
@@ -35,5 +42,50 @@ public class UserRestController {
     @ApiOperation(value = "Retrieves users by the entered school name")
     public Flux<User> getUsersBySchoolName(@PathVariable("schoolName") String schoolName) {
         return repository.findAllBySchoolName(schoolName);
+    }
+
+    @PostMapping("/api/users/new")
+    @ApiOperation(value = "Creates a new user with the given parameters")
+    public Mono<String> editUser(
+            @RequestParam(value = "username") String username,
+            @RequestParam(value = "password") String password,
+            @RequestParam(value = "school") String schoolName,
+            @RequestParam(value = "roles", required = false) Optional<List<String>> roles
+    )
+    {
+        User newUser = new User(username, passwordEncoder.encode(password), schoolName);
+        roles.ifPresent(roleList -> roleList.forEach(role -> newUser.getRoles().add(new SimpleGrantedAuthority(role))));
+        return repository.save(newUser)
+                .log("User Creation")
+                .then(Mono.just("Successfully Created!"));
+    }
+
+    @PostMapping("/api/users/edit")
+    @ApiOperation(value = "Edits a user with the given parameters")
+    public Mono<String> editUser(
+            @RequestParam(value = "id") String id,
+            @RequestParam(value = "username", required = false) Optional<String> newUsername,
+            @RequestParam(value = "password", required = false) Optional<String> newPassword,
+            @RequestParam(value = "school", required = false) Optional<String> newSchoolName,
+            @RequestParam(value = "roles", required = false) Optional<List<String>> newRoles
+    )
+    {
+        return repository.findById(id)
+                .log("Updating User")
+                .map(user -> {
+                    newUsername.ifPresent(user::setUsername);
+                    newPassword.ifPresent(password -> user.setPassword(passwordEncoder.encode(password)));
+                    newSchoolName.ifPresent(user::setSchoolName);
+                    newRoles.ifPresent(roleList -> roleList.forEach(role -> user.getRoles().add(new SimpleGrantedAuthority(role))));
+                    return user;
+                })
+                .flatMap(repository::save)
+                .then(Mono.just("Successfully Updated!"));
+    }
+
+    @DeleteMapping("/api/users/delete/{id}")
+    @ApiOperation(value = "Deletes a user with the provided id")
+    public Mono<String> newUser(@PathVariable("id") String id) {
+        return repository.deleteById(id).then(Mono.just("Successfully Deleted!"));
     }
 }
